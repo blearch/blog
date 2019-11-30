0dc144bf7c9c5cb34f117075b52123591d911b25
blearch <1801294368@qq.com>
2019-11-30 16:04:00 +0800

    nothing

--- article/admin.py
+++ article/admin.py
@@ -1,3 +1,6 @@
+from article.models import ArticlePost
 from django.contrib import admin
 
 # Register your models here.
+# 把数据库表注册到管理中
+admin.site.register(ArticlePost)
--- article/models.py
+++ article/models.py
@@ -12,7 +12,7 @@ from django.utils import timezone
 
 class ArticlePost(models.Model):
     # 文章作者，on_delete表示删除方式
-    # Foreignkey用来解决一对多的关系  oneToOneField用来一对一  ManyToManyField用来解决多对多
+    # Foreignkey用来解决一对多的关系  oneToOneField用来一对一  ManyToManyField用来解决多对多 选择的用户是管理员用户
     author=models.ForeignKey(User,on_delete=models.CASCADE)
 
     # 文章标题
@@ -20,7 +20,7 @@ class ArticlePost(models.Model):
     # 文章正文
     body=models.TextField()
 #     文章创建的时间  参数default=timezone.now 指定其在创建数据时将默认写入当前的时间
-    created=models.DateTimeField(default=timezone.now)
+    created=models.DateTimeField(default=timezone.localtime)
 #     文章更新时间。参数auto_now=True指定每次数据更新字典填写当前时间
     updated=models.DateTimeField(auto_now=True)
     # meta类相当于对原来的类进行功能的添加。主要是为模型提供元数据。
