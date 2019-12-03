e638d7add0f1e03dd01a4b0ecb73f3a62a665a51
blearch <1801294368@qq.com>
2019-12-03 17:56:46 +0800

    nothing

--- article/urls.py
+++ article/urls.py
@@ -6,4 +6,6 @@ app_name='article'
 urlpatterns = [
     # path函数将url映射到视图
     path('article-list/', views.article_list, name='article-list'),
+#     文章详情
+    path('article-detail/<int:id>/',views.article_detail,name='article_detail'),
 ]
